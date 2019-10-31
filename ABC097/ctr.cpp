1e76728a0f7f649113335a35622c92453cd97662
Deepndra Chourasia <36853145+rkstar1207@users.noreply.github.com>
2019-10-31 20:13:43 +0530

    use using namespace

--- ABC097/ctr.cpp
+++ ABC097/ctr.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 int main(void)
 {
 	int a,b,c,d;
-	std::cin>>a>>b>>c>>d;
+	cin>>a>>b>>c>>d;
 	if((abs(a-c) <= d) || ((abs(a-b) <= d) && (abs(b-c) <= d)))
 	std::cout<<"Yes\n";
 	else std::cout<<"No\n";
