bad566579966f9586a91b2d80d04f479f5f60b58
Deepndra Chourasia <36853145+rkstar1207@users.noreply.github.com>
2019-10-31 20:38:19 +0530

    acod.cpp

--- ACODE/acode.cpp
+++ ACODE/acode.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
-
-int memo[5001];
+#define ll long long int
+ll memo[5001];
 std::string s;
 
-int acx(int n)
+ll acx(int n)
 {
 	if(n == (int)s.size())
 	return 1;
 	if(memo[n] != -1)
 	return memo[n];
-	int ans = 0;
+	ll ans = 0;
 	if(s[n] != '0')
 	ans += acx(n+1);
 	if((n+1 < (int)s.size()) && ((s[n] == '1') || ((s[n] == '2') && (s[n+1] >= '0') && (s[n+1] <= '6'))))
@@ -27,7 +27,7 @@ int main(void)
 	while(s[0] != '0')
 	{
 		memset(memo, -1, sizeof(memo));
-		int ans = acx(0);
+		ll ans = acx(0);
 		std::cout<<ans<<"\n";
 		std::cin>>s;
 	}
